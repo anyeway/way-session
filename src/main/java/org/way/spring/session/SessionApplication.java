3351f1020279eec4f7d9f316e783aa26bcabe49c
wengay <wengay@101.com>
2016-03-04 09:26:16 +0800

    太久了 忘记了

---
+++ src/main/java/org/way/spring/session/SessionApplication.java
@@ -0,0 +1,16 @@
+package org.way.spring.session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author way
+ *         Created on 2016/1/12.
+ */
+@SpringBootApplication
+public class SessionApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SessionApplication.class, args);
+    }
+}
